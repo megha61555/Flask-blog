a3fe44db7ebffdf9ccfe8ca0e910ef9e401b8218
megha61555 <megha61555@gmail.com>
2025-11-15 12:14:54 +0530

    Remove venv from Git

---
+++ app.py
@@ -0,0 +1,88 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '87da2ec21cba208505b1f39fdf39b68a'
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024  # 2MB max file size
+
+db = SQLAlchemy(app)
+
+# Database model
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    image = db.Column(db.String(100))  # store image filename
+
+# Home page: List all posts
+@app.route('/')
+def index():
+    posts = Post.query.order_by(Post.id.desc()).all()
+    return render_template('index.html', posts=posts)
+
+# View single post
+@app.route('/post/<int:id>')
+def post(id):
+    post = Post.query.get_or_404(id)
+    return render_template('post.html', post=post)
+
+# Add new post
+@app.route('/add', methods=['GET', 'POST'])
+def add_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        file = request.files.get('image')
+        filename = None
+        if file and file.filename != '':
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        new_post = Post(title=title, content=content, image=filename)
+        db.session.add(new_post)
+        db.session.commit()
+        flash("Post added successfully!", "success")
+        return redirect('/')
+    return render_template('add_post.html')
+
+# Edit post
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit_post(id):
+    post = Post.query.get_or_404(id)
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.content = request.form['content']
+        file = request.files.get('image')
+        if file and file.filename != '':
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            post.image = filename
+        db.session.commit()
+        flash("Post updated successfully!", "success")
+        return redirect('/')
+    return render_template('edit_post.html', post=post)
+
+# Delete post
+@app.route('/delete/<int:id>')
+def delete_post(id):
+    post = Post.query.get_or_404(id)
+    if post.image:
+        try:
+            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], post.image))
+        except:
+            pass
+    db.session.delete(post)
+    db.session.commit()
+    flash("Post deleted successfully!", "danger")
+    return redirect('/')
+
+if __name__ == '__main__':
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+    with app.app_context():
+        db.create_all()
+    #app.run(debug=True)---to run locally
+     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000))) #to run online
